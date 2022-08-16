3d422e3b7fc39e1bc8bc4ebbfbbb08f954673e86
SieR-VR <nwh040512@gmail.com>
2022-08-16 13:09:47 +0900

    vcpkg init

--- build.sh
+++ build.sh
@@ -1,5 +1,11 @@
 # git submodule install
 git submodule update --init --recursive
 
+echo "
+set(VCPKG_TARGET_ARCHITECTURE arm64)
+set(VCPKG_CRT_LINKAGE dynamic)
+set(VCPKG_LIBRARY_LINKAGE dynamic)
+set(VCPKG_CMAKE_SYSTEM_NAME Android)
+" > $VCPKG_ROOT/triplets/community/arm64-android.cmake
 
 ./gradlew assembleDebug
