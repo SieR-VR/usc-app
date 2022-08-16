9e391b8a7f958cdff009746e44418e4297b45864
SieR-VR <nwh040512@gmail.com>
2022-08-16 13:32:57 +0900

    asdf

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ set(VCPKG_TARGET_ARCHITECTURE arm64)
 set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_LIBRARY_LINKAGE dynamic)
 set(VCPKG_CMAKE_SYSTEM_NAME Android)
-" > $VCPKG_ROOT/triplets/community/arm64-android.cmake
+" > /home/runner/work/usc-app/usc-app/vcpkg/triplets/community/arm64-android.cmake
 
 ./gradlew assembleDebug
