7710e7b360eaafafa84da24edb2f902e4a5df598
SieR-VR <nwh63@naver.com>
2022-08-24 18:40:13 +0900

    update build.sh

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ set(VCPKG_TARGET_ARCHITECTURE arm64)
 set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_LIBRARY_LINKAGE dynamic)
 set(VCPKG_CMAKE_SYSTEM_NAME Android)
-set(VCPKG_C_FLAGS -std=c11)
+set(VCPKG_C_FLAGS -std=gnu11)
 set(VCPKG_CXX_FLAGS -std=gnu++17)
 " > ./vcpkg/triplets/community/arm64-android.cmake
 
