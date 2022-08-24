783fc7c45b5b772f2435d21af4fa5f8d1b2849ad
SieR-VR <nwh63@naver.com>
2022-08-24 17:49:42 +0900

    update build.sh

--- build.sh
+++ build.sh
@@ -6,8 +6,8 @@ set(VCPKG_TARGET_ARCHITECTURE arm64)
 set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_LIBRARY_LINKAGE dynamic)
 set(VCPKG_CMAKE_SYSTEM_NAME Android)
-set(VCPKG_C_FLAGS -std=c99)
-set(VCPKG_CXX_FLAGS -std=c++17)
+set(VCPKG_C_FLAGS \${VCPKG_C_FLAGS} -std=c99)
+set(VCPKG_CXX_FLAGS \${VCPKG_CXX_FLAGS} -std=c++17)
 " > ./vcpkg/triplets/community/arm64-android.cmake
 
 export ANDROID_NDK_HOME="/home/sier/android-sdk/ndk/21.4.7075529"
