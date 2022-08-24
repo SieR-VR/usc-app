7e42bf0a8f56dcf35285d375330863ef219bd085
SieR-VR <nwh63@naver.com>
2022-08-24 17:44:28 +0900

    I'm praing

--- build.sh
+++ build.sh
@@ -6,8 +6,10 @@ set(VCPKG_TARGET_ARCHITECTURE arm64)
 set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_LIBRARY_LINKAGE dynamic)
 set(VCPKG_CMAKE_SYSTEM_NAME Android)
-" > /home/runner/work/usc-app/usc-app/vcpkg/triplets/community/arm64-android.cmake
+set(VCPKG_C_FLAGS -std=c99)
+set(VCPKG_CXX_FLAGS -std=c++17)
+" > ./vcpkg/triplets/community/arm64-android.cmake
+
+export ANDROID_NDK_HOME="/home/sier/android-sdk/ndk/21.4.7075529"
 
 ./gradlew assembleDebug
-
-cat /home/runner/work/usc-app/usc-app/vcpkg/buildtrees/sdl2/install-arm64-android-dbg-out.log
