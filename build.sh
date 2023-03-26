d1bf122f2893d82c32d11d55b75f558baa9834c9
SieR-VR <nwh040512@gmail.com>
2023-03-26 13:50:43 +0900

    for test

--- build.sh
+++ build.sh
@@ -7,3 +7,5 @@ set(VCPKG_LIBRARY_LINKAGE static)
 " > ./vcpkg/triplets/community/arm64-android.cmake
 
 ./gradlew assembleDebug
+
+cat "/home/runner/work/usc-app/usc-app/vcpkg/buildtrees/detect_compiler/config-${VCPKG_TARGET_TRIPLET}-rel-err.log"
